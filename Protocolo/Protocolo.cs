1053496c4d9f3c8ffc1f9b6a77a1a327688da1eb
DIANA LIZBETH RODRIGUEZ CARAPAZ <diana.rodriguez@epn.edu.ec>
2025-11-25 22:32:09 -0500

    Se agrego una nueva clase Protocolo que implemente: ResolverPedido(Pedido pedido) HazOperacion(Pedido pedido) Esta clase será usada por el cliente para enviar datos al servidor y por el servidor para procesar la lógica.

--- Protocolo/Protocolo.cs
+++ Protocolo/Protocolo.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
 
 namespace Protocolo
 {
@@ -34,4 +37,58 @@ namespace Protocolo
         }
     }
 
-}
+    public class Protocolo
+    {
+        private TcpClient cliente;
+        private NetworkStream flujo;
+
+        public Protocolo(string ip, int puerto)
+        {
+            cliente = new TcpClient(ip, puerto);
+            flujo = cliente.GetStream();
+        }
+    
+    
+    public Respuesta ResolverPedido(Pedido pedido)
+        {
+            return HazOperacion(pedido);
+        }
+
+        private Respuesta HazOperacion(Pedido pedido)
+        {
+            try
+            {
+                string mensaje = pedido.ToString();
+                byte[] bufferTx = Encoding.UTF8.GetBytes(mensaje);
+
+                flujo.Write(bufferTx, 0, bufferTx.Length);
+
+                byte[] bufferRx = new byte[1024];
+                int bytesRx = flujo.Read(bufferRx, 0, bufferRx.Length);
+
+                string respuestaStr = Encoding.UTF8.GetString(bufferRx, 0, bytesRx);
+
+                var partes = respuestaStr.Split(' ');
+                return new Respuesta
+                {
+                    Estado = partes[0],
+                    Mensaje = string.Join(" ", partes.Skip(1).ToArray())
+                };
+            }
+            catch (SocketException ex)
+            {
+
+                throw new Exception("Error en la comunicación: " + ex.Message);
+            }
+        }
+
+        public void CerrarConexion()
+        {
+            if (flujo != null)
+                flujo.Close();
+            if (cliente != null)
+                cliente.Close();
+        }
+    }
+
+ }
